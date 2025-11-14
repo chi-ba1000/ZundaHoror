66eae6e51adae64ebd78b6f2494239ba3b502e85
chi-ba1000 <chiba.1000.takuma@gmail.com>
2025-11-15 00:39:07 +0900

    prefab

---
+++ Assets/Script/reactiveObjectHondana.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+
+public class reactiveObjectHondana : MonoBehaviour
+{
+    //このスクリプトはドアオブジェクトにくっつけて（自機にplayer、敵にenemyというタグをつけて）
+    public float openDistance = 1f;
+    private Vector3 closePos;
+    
+    private void OnTriggerStay(Collider other)
+    {
+        Transform doorbasis = transform.parent.parent;
+        closePos = doorbasis.position;
+
+
+        if (((other.CompareTag("Player") && Input.GetKey(KeyCode.F) )|| other.CompareTag("Enemy")))
+        {
+            StartCoroutine(DoorOpen());
+            StartCoroutine(DoorHit());
+        }
+    }
+    private IEnumerator DoorOpen()
+    {
+        Debug.Log("開く");
+        Debug.Log(closePos);
+        Transform doorbasis = transform.parent.parent;
+        float openTime = 0.5f;
+        float waitTime = 0f;
+        Vector3 openPos = closePos + new Vector3(openDistance, 0, 0);
+
+        while (waitTime < openTime)
+        {
+            waitTime += Time.deltaTime;
+            float openState = waitTime / openTime;
+            
+            //Lerpは線形補完、一つ目の要素と二つ目の要素の間を３つ目の要素の状態で移動
+            doorbasis.position = Vector3.Lerp(closePos, openPos, openState);
+
+            yield return null;
+        }
+    }
+
+
+    private IEnumerator DoorHit()
+    {
+        GameObject doorslide = transform.parent.gameObject;
+        foreach (Transform children in doorslide.transform)
+        {
+            Collider col = children.GetComponent<Collider>();
+            col.enabled = false;
+        }
+        yield return new WaitForSeconds(2.1f);
+        foreach (Transform children in doorslide.transform)
+        {
+            Collider col = children.GetComponent<Collider>();
+            col.enabled = true;
+        }
+        yield return null;
+    }
+}
